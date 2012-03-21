b12791be537b842677b62dcb45fe8674d29a5a6b
Brennon Loveless <brennon@fritzandandre.com>
2012-03-21 02:12:15 -0600

    First actual implementation of creating and getting groups, as well as sending emails NEED TO DOCUMENT THE FUNCTIONS

--- README.md
+++ README.md
@@ -16,6 +16,28 @@ Or install it yourself as:
 
     $ gem install jangosmtp
 
+## Configuration
+
+In your environment file there are a few options that you can pass to Jangosmtp as follows:
+
+After the gem is installed you will need to add your jango configuration to your environment file. Using the following lines of code. Pay close attention to the comments here.
+
+    Jangosmtp.options({
+      username: 'username',
+      password: 'password',
+      max_attempts: 1,
+      click_tracking: true,
+      open_tracking: true,
+      auto_generate_plain: true
+    })
+
+* username is required and is the same username you use to login to jangosmtp.
+* password is required and is the same password you use to login to jangosmtp.
+* max_attempts is _not_ required, but if there is a failed call to jangosmtp the gem will attempt the try again every three seconds until max_attempts is reached. There is a catch here, jangosmtp relies on 500 internal server errors even when a value is listed incorrectly, so this will slow down your application. For example: when creating a groups this gem tries to get the group first. If jangosmtp does not find the requested group then it will throw a 500 internal server error. Which will cause the system to try again until the max number of attempts are reached before attempting to create the group. I'm working with jangosmtp for a fix to this issue.
+* click_tracking is _not_ required but will default to true which enables tracking for whenever anyone clicks on any link that is included in your email.
+* open_tracking is _not_ required but will default to true which enables tracking for whenever anyone opens an email you sent, this is done by jangosmtp including a transparent image in your email so they can monitor when the image is downloaded from their server.
+* auto_generate_plain is _not_ required but will default to true which tells jangosmtp that you are sending html content for your email and that jangosmtp should generate the plain text version of the email to be sent along with your email, this is recommended since this gem doesn't currently have the ability to send plain text emails
+
 ## Usage
 
 TODO: Write usage instructions here
--- lib/jangosmtp.rb
+++ lib/jangosmtp.rb
@@ -1,7 +1,125 @@
-require "jangosmtp/version"
+require 'jangosmtp/version'
 
 module Jangosmtp
-  def sef.say_something
-    'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  
+  class << self
+    attr_accessor :username, :password, :max_attempts, :click_tracking, :open_tracking, :auto_generate_plain
+    BASE_URL = 'http://api.jangomail.com/api.asmx/'
+    
+    def options( hash )
+      @username = hash[:username] if !hash[:username].nil?
+      @password = hash[:password] if !hash[:password].nil?
+      @max_attempts = hash[:max_attempts] ||= 1
+      @click_tracking = hash[:click_tracking] ||= true
+      @open_tracking = hash[:open_tracking] ||= true
+      @auto_generate_plain = hash[:open_tracking] ||= true
+    end
+    
+    def get_create_group( group_name )
+      check_user_pass
+      existing_group_id = get_group_id( group_name )
+      if existing_group_id == nil
+        new_group_id = create_group( group_name )
+        return new_group_id
+      end
+      return existing_group_id
+    end
+    
+    def get_group_id( group_name )
+      check_user_pass
+      # First we need to clean the group_name since jangosmtp only allows alphanumeric characters
+      group_name.tr!('^A-Za-z0-9 ', '')
+      options = {
+        'Username' => @username,
+        'Password' => @password,
+        'GroupName' => group_name
+      }
+    
+      # First we are going to check the existing groups to make sure that the current group doesn't already exist.
+      found_group = false
+      existing_group_id = nil
+      response = post_with_attempts( "GetTransactionalGroupID", options )
+      if response != false
+        existing_group_id = Nokogiri::XML.parse(response.body).xpath("*").first.content.split("\n")[2]
+        found_group = true
+      end
+    
+      return existing_group_id
+    end
+    
+    def create_group( group_name )
+      check_user_pass
+      # First we need to clean the group_name since jangosmtp only allows alphanumeric characters
+      group_name.tr!('^A-Za-z0-9 ', '')
+      options = {
+        'Username' => @username,
+        'Password' => @password,
+        'GroupName' => group_name
+      }
+      
+      response = post_with_attempts( 'AddTransactionalGroup', options )
+      if response != false
+        new_group_id = Nokogiri::XML.parse(response.body).xpath("*").first.content.split("\n")[2]
+      end
+      return new_group_id
+    end
+    
+    def send_email( group_name, to_email, from_email, from_name, html )
+      check_user_pass
+      group_id = get_create_group( group_name )
+      unless group_id.nil?
+        return send_email_with_group_id( group_id, to_email, from_email, from_name, html )
+      end
+    end
+    
+    def send_email_with_group_id( group_id, to_email, from_email, from_name, html )
+      check_user_pass
+      # Send the email using Jango
+      options = {
+        'Username' => @username,
+        'Password' => @password,
+        'FromEmail' => from_email,
+        'FromName' => from_name,
+        'ToEmailAddress' => to_email,
+        'Subject' => 'SOCO Authorization Request',
+        'MessagePlain' => 'auto-generate',
+        'MessageHTML' => html,
+        'Options' => 'OpenTrack=' + @open_tracking.to_s + ',ClickTrack=' + @click_tracking.to_s + ',TransactionalGroupID=' + group_id
+      }
+      return post_with_attempts( 'SendTransactionalEmail', options )
+    end
+    
+    private
+    def check_user_pass
+      if @username.nil? || @password.nil?
+        raise 'Jangosmtp username and password are required'
+      end
+    end
+    
+    def post_with_attempts( action, options )
+      agent = Mechanize.new
+      attempt = 0
+      response = false
+      # Try max_attempts times before skipping
+      while((attempt < max_attempts) && !response)
+        begin
+          response = agent.post( BASE_URL + action, options )
+        rescue StandardError => e
+          # If there was an error set success to false and try again in 3 seconds
+          response = false
+          attempt += 1
+          sleep 3
+        end
+      end
+      return response
+    end
+    
+    def logger
+      if !Rails.nil?
+        return Rails.logger
+      else
+        return Logger.new( STDOUT )
+      end
+    end
   end
 end
