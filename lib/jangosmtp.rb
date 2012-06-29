2794e8ef839c3e99325c7c6e0c5f6748da8c5b55
Brennon Loveless <brennon@fritzandandre.com>
2012-06-29 14:57:17 -0600

    Added a necessary require to the top of the lib file

--- lib/jangosmtp.rb
+++ lib/jangosmtp.rb
@@ -1,11 +1,12 @@
 require 'jangosmtp/version'
+require 'mechanize'
 
 module Jangosmtp
-  
+
   class << self
     attr_accessor :username, :password, :max_attempts, :click_tracking, :open_tracking, :auto_generate_plain
     BASE_URL = 'http://api.jangomail.com/api.asmx/'
-    
+
     def options( hash )
       @username = hash[:username] if !hash[:username].nil?
       @password = hash[:password] if !hash[:password].nil?
@@ -14,7 +15,7 @@ module Jangosmtp
       @open_tracking = hash[:open_tracking] ||= true
       @auto_generate_plain = hash[:open_tracking] ||= true
     end
-    
+
     # Get a group if the group exists, otherwise create a group using a group name and return it's id
     # group_name: group name that the user wants to get the id for, should only contain alphanumeric ( and spaces )
     #   will be cleaned if an incorrect name is used
@@ -27,7 +28,7 @@ module Jangosmtp
       end
       return existing_group_id
     end
-    
+
     # Get the id of a requested group
     # group_name: group name that the user wants to get the id for, should only contain alphanumeric ( and spaces )
     #   will be cleaned if an incorrect name is used
@@ -40,7 +41,7 @@ module Jangosmtp
         'Password' => @password,
         'GroupName' => group_name
       }
-    
+
       # First we are going to check the existing groups to make sure that the current group doesn't already exist.
       found_group = false
       existing_group_id = nil
@@ -49,10 +50,10 @@ module Jangosmtp
         existing_group_id = Nokogiri::XML.parse(response.body).xpath("*").first.content.split("\n")[2]
         found_group = true
       end
-    
+
       return existing_group_id
     end
-    
+
     # Create a group and return the successfull value
     # group_name: group name that the user wants to get the id for, should only contain alphanumeric ( and spaces )
     #   will be cleaned if an incorrect name is used
@@ -65,14 +66,14 @@ module Jangosmtp
         'Password' => @password,
         'GroupName' => group_name
       }
-      
+
       response = post_with_attempts( 'AddTransactionalGroup', options )
       if response != false
         new_group_id = Nokogiri::XML.parse(response.body).xpath("*").first.content.split("\n")[2]
       end
       return new_group_id
     end
-    
+
     # Send an email and get/create a requested group
     # group_name: group name that the user wants the email to be applied to, should only contain alphanumeric
     #   ( and spaces ) will be cleaned if an incorrect name is used
@@ -93,7 +94,7 @@ module Jangosmtp
         return send_email_with_group_id( group_id, subject, to_email, from_email, from_name, html )
       end
     end
-    
+
     # Send an email using a pre-existing group
     # group_id: the id of the group that this email will be applied to
     # subject: the subject of the email to be sent
@@ -116,7 +117,7 @@ module Jangosmtp
       }
       return post_with_attempts( 'SendTransactionalEmail', options )
     end
-    
+
     private
     # Will verify that the username and password exist for each request
     def check_user_pass
@@ -124,7 +125,7 @@ module Jangosmtp
         raise 'Jangosmtp username and password are required'
       end
     end
-    
+
     # Will attempt to post to the jangosmtp action requested using the options hash passed in
     def post_with_attempts( action, options )
       agent = Mechanize.new
@@ -143,7 +144,7 @@ module Jangosmtp
       end
       return response
     end
-    
+
     # Get either the Rails logger or the a logger to STDOUT
     def logger
       if !Rails.nil?
